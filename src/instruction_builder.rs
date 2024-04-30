0c34777dd210467cc4c55d6635bc0fe998d621b6
levente kiraly <leventekk@users.noreply.github.com>
2024-04-30 08:27:25 +0200

    feat(instruction_builder): add COMMIT_OPTIONAL_BODY constant
    - added COMMIT_OPTIONAL_BODY constant to provide guidance on including changes that cannot fit into the description

--- src/instruction_builder.rs
+++ src/instruction_builder.rs
@@ -8,6 +8,13 @@ The user provides a `git diff` format for you and your task is to generate a com
 static COMMIT_GUIDANCE: &str = r"
 Some guidance for generating the commit:
 - Use the present tense. Lines must not be longer than 74 characters.
+- Do not start the sentences with a capital letter, use small case everywhere
+- Do not add any extra formatting to the commit message
+- Remove the starting and ending backticks and also the new lines
+- Focus on code changes and the reason why those changes were made
+";
+
+static COMMIT_OPTIONAL_BODY: &str = r"
 - As for the [optional body], you can include all the changes that cannot fit into the <description>
 - If you're using a list to mention all of the changes in the [optional body], for example:
 ```
@@ -15,10 +22,6 @@ Some guidance for generating the commit:
 - fixed a bug when the list is empty
 ```
 - If there is only one item that can be listed in the [optional body] you can write it as a sentence, no need to use a dash
-- Do not start the sentences with a capital letter, use small case everywhere
-- Do not add any extra formatting to the commit message
-- Remove the starting and ending backticks and also the new lines
-- Focus on code changes and the reason why those changes were made
 ";
 
 pub struct InstructionBuilder {}
@@ -30,6 +33,7 @@ impl InstructionBuilder {
 			INITIAL_PROMPT_INSTRUCTION,
 			instruction_strategy,
 			COMMIT_GUIDANCE,
+            COMMIT_OPTIONAL_BODY,
 		]
 		.join("\n")
 	}
