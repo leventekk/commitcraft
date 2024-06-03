e0d99e6aea67d9fc48a40132fd16cf09cd8cabe8
levente kiraly <leventekk@users.noreply.github.com>
2024-06-03 16:18:37 +0200

    chore(tests): add some basic test cases for executor

--- src/executor.rs
+++ src/executor.rs
@@ -1,5 +1,4 @@
 use crate::prelude::*;
-
 use std::process::Command;
 
 pub struct Executor {}
@@ -7,19 +6,41 @@ pub struct Executor {}
 impl Executor {
 	pub fn execute(arguments: Vec<&str>) -> Result<String> {
 		let output = Command::new("git").args(&arguments).output()?;
-		let convert_error =
+		let err_convert =
 			Error::Executor("Could not convert output to string".to_string());
 
+        // if there is an error while executing the command, we need to print the error message.
+        // for example: if the the pre-commit hook found some errors.
 		if !output.status.success() {
 			let error_message =
-				String::from_utf8(output.stderr).map_err(|_| convert_error)?;
+				String::from_utf8(output.stderr).map_err(|_| err_convert)?;
 
 			return Err(Error::Executor(error_message));
 		}
 
 		let response =
-			String::from_utf8(output.stdout).map_err(|_| convert_error)?;
+			String::from_utf8(output.stdout).map_err(|_| err_convert)?;
 
 		Ok(response)
 	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_execute() {
+		let arguments = vec!["--version"];
+		let response = Executor::execute(arguments).unwrap();
+		assert!(response.contains("git version"));
+	}
+
+    #[test]
+    fn test_execute_error() {
+        let arguments = vec!["missing command"];
+        let response = Executor::execute(arguments);
+
+        assert!(response.is_err());
+    }
 }
